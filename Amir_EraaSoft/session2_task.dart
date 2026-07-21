bb0413daf453509853d03ec533aff52a3b8de1e8
Amir <amiraymin6066@gmail.com>
2026-07-21 21:05:04 +0300

    session2_task

---
+++ Amir_EraaSoft/session2_task.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Q1 : create a variable called hour and assign your current hour to it
+  double hour = 8.50;
+
+  // Q2 : create two variables fristname and lastname then print them
+  String firstName = "Amir";
+  String lastName = "Aymin";
+  print(firstName + lastName);
+
+  // Q3 : create a double variable named "kilogram" and assign your weight
+  int kilogram = 70;
+
+  // Q4 : create a bool variable named "isStudent" and assign it
+  bool isStudent = false;
+
+  // Q5 : create a var to declare a variable called "city" and assign your city name then print it
+  String city = "alexandria";
+  print(city);
+}
